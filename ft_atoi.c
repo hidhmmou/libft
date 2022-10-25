8527fd3fa9818462b595bb9d1d8fe9a85d745f34
KILLUA <115652156+ft-killua@users.noreply.github.com>
2022-10-25 01:45:38 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,50 +12,71 @@
 
 #include "libft.h"
 
-int	ft_isspace(const char c)
+static int	ft_isspace(const char t)
 {
-	if (c == ' '
-		|| c == '\t'
-		|| c == '\v'
-		|| c == '\n'
-		|| c == '\f'
-		|| c == '\r')
+	if (t == ' ')
+		return (1);
+	if (t >= 9 && t <= 13)
 		return (1);
 	return (0);
 }
 
-int	ft_isnum(const char c)
+static int	is_num(const char t)
 {
-	if (c >= '0' && c <= '9')
+	if (t >= '0' && t <= '9')
 		return (1);
 	return (0);
+}
+
+static int	ft_check(int m, const char *str, int sign)
+{
+	if (m >= 19)
+	{
+		if (ft_strncmp("9223372036854775807", str, 19) < 0
+			&& sign == 1)
+			return (-1);
+		if (ft_strncmp("9223372036854775808", str, 19) < 0
+			&& sign == -1)
+			return (0);
+	}
+	return (1);
+}
+
+static void	skip_and_inicialise(int *i, int *sign, const char *str)
+{
+	*sign = 1;
+	*i = 0;
+	while (str[*i] && ft_isspace(str[*i]))
+		(*i)++;
+	if (str[*i] == '+')
+		(*i)++;
+	else if (str[*i] == '-')
+	{
+		(*i)++;
+		*sign = -1;
+	}
+	while (str[*i] == '0')
+		(*i)++;
 }
 
 int	ft_atoi(const char *str)
 {
-	int		i;
+	int					i;
 	unsigned long long	s;
-	int		sign;
+	int					sign;
+	int					m;
+	int					start;
 
-	i = 0;
+	skip_and_inicialise(&i, &sign, str);
+	m = 0;
 	s = 0;
-	sign = 1;
-	while (str[i] && ft_isspace(str[i]))
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{	
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (str[i] && ft_isnum(str[i]))
+	start = i;
+	while (str[i] && is_num(str[i]))
 	{
-		s = s * 10 + (str[i] - '0');
-		i++;
-		if (s * sign < LLONG_MAX)
-			return (0);
-		if (s * sign > LLONG_MIN)
-			return (-1);
+		s = s * 10 + str[i++] - '0';
+		m++;
+		if (ft_check(m, str + start, sign) != 1)
+			return ((ft_check(m, str + start, sign)));
 	}
 	return (sign * s);
 }
