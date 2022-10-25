fd031e411beba75cbdf93e50f49c5e009bb07e1d
KILLUA <115652156+ft-killua@users.noreply.github.com>
2022-10-25 01:46:16 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,22 +12,23 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	len_d;
 	size_t	len_s;
 
 	i = 0;
-	len_d = ft_strlen(dest);
 	len_s = ft_strlen(src);
+	if (dst)
+		len_d = ft_strlen(dst);
 	if (len_d >= dstsize || dstsize == 0)
 		return (dstsize + len_s);
 	while (src[i] && i < dstsize - len_d - 1)
 	{
-		dest[len_d + i] = src[i];
+		dst[len_d + i] = src[i];
 		i++;
 	}
-	dest[len_d + i] = '\0';
+	dst[len_d + i] = '\0';
 	return (len_d + len_s);
 }
